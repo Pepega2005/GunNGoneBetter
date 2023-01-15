d3ebc017e848affa56c4144ded4de268f64be46e
Pepega2005 <75209223+Pepega2005@users.noreply.github.com>
2023-01-15 19:49:58 +0200

    Cart improved

--- GunNGoneBetter/Controllers/CartController.cs
+++ GunNGoneBetter/Controllers/CartController.cs
@@ -1,13 +1,19 @@
 ﻿using GunNGoneBetter.Data;
 using GunNGoneBetter.Models;
+using GunNGoneBetter.Models.ViewModels;
 using GunNGoneBetter.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace GunNGoneBetter.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         ApplicationDbContext db;
+
+        ProductUserViewModel productUserVIewModel;
 
         public CartController(ApplicationDbContext db)
         {
@@ -38,8 +44,48 @@ namespace GunNGoneBetter.Controllers
         public IActionResult Remove(int id)
         {
             // удаление из корзины
+            List<Cart> cartList = new List<Cart>();
+
+            if (HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart).Count() > 0)
+            {
+                cartList = HttpContext.Session.Get<List<Cart>>(PathManager.SessionCart);
+            }
+
+            cartList.Remove(cartList.FirstOrDefault(x => x.ProductId == id));
+
+            HttpContext.Session.Set(PathManager.SessionCart, cartList);
 
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+
+            // если ипользователь вошел в систему, то обьект будет определен
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            List<Cart> cartList = new List<Cart>();
+
+            if (HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<Cart>>(PathManager.SessionCart).Count() > 0)
+            {
+                cartList = HttpContext.Session.Get<List<Cart>>(PathManager.SessionCart);
+            }
+
+            List<int> productsIdInCart = cartList.Select(x => x.ProductId).ToList();
+
+            IEnumerable<Product> productList = db.Product.Where(x => productsIdInCart.Contains(x.Id));
+
+            productUserVIewModel = new ProductUserViewModel()
+            {
+                ApplicationUser = db.ApplicationUser.FirstOrDefault(x => x.Id == claim.Value),
+                ProductList = productList
+            };
+
+            return View(productUserVIewModel);
         }
     }
 }
---
+++ GunNGoneBetter/Models/ViewModels/ProductUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GunNGoneBetter.Models.ViewModels
+{
+    public class ProductUserViewModel
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Product> ProductList { get; set; }
+
+        public ProductUserViewModel()
+        {
+            ProductList = new List<Product>();
+        }
+    }
+}
